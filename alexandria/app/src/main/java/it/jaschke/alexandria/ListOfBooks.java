30484ad8c6cb548235181f4f08aeac8dab0661c4
Chris Olsen <olsen.chris@gmail.com>
2015-10-26 23:03:52 -0600

    hide the keyboard after selecting a book

--- alexandria/app/src/main/java/it/jaschke/alexandria/ListOfBooks.java
+++ alexandria/app/src/main/java/it/jaschke/alexandria/ListOfBooks.java
@@ -1,6 +1,7 @@
 package it.jaschke.alexandria;
 
 import android.app.Activity;
+import android.content.Context;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -10,6 +11,7 @@ import android.support.v4.content.Loader;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.SearchView;
@@ -78,6 +80,9 @@ public class ListOfBooks extends Fragment implements LoaderManager.LoaderCallbac
                 if (cursor != null && cursor.moveToPosition(position)) {
                     ((Callback)getActivity())
                             .onItemSelected(cursor.getString(cursor.getColumnIndex(AlexandriaContract.BookEntry._ID)));
+
+                    InputMethodManager imm = (InputMethodManager) getActivity().getSystemService(Context.INPUT_METHOD_SERVICE);
+                    imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
                 }
             }
         });
